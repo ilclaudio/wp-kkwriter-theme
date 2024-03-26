e9aa3261b625c90abfe84e86ec80db1cc7b9d6ec
claudio.battaglino@sns.it <ilclaudio@gmail.com>
2024-03-26 23:46:18 +0100

    NewFeature: Added library to manage theme dependencies.

---
+++ config_theme.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * KK Writer Theme: Theme configuration constants.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package KK_Writer_Theme
+ */
+
+define(
+	'SUGGESTED_PLUGINS',
+	array(
+		// array(
+		// 	'name'     => 'Really Simple CAPTCHA',
+		// 	'slug'     => 'really-simple-captcha',
+		// 	'required' => true,
+		// ),
+	)
+);
--- functions.php
+++ functions.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * KK Writer Theme: Functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package KK_Writer_Theme
+ */
 
-function load_scripts_and_styles()
+/**
+ * Define the theme parameters and configurations.
+ */
+require get_template_directory() . '/config_theme.php';
+
+/**
+ * Implement Plugin Activations Rules.
+ */
+require get_template_directory() . '/inc/theme-dependencies.php';
+
+function kkw_load_scripts_and_styles()
 {
 	// Add bootstrap files.
 	wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css' );
@@ -12,5 +29,4 @@ function load_scripts_and_styles()
 	wp_register_style( 'kkwritertheme_main_styles', get_template_directory_uri() . '/assets/custom/css/main.css' );
 	wp_enqueue_style( 'kkwritertheme_main_styles' );
 }
-
-add_action( 'wp_enqueue_scripts', 'load_scripts_and_styles' );
+add_action( 'wp_enqueue_scripts', 'kkw_load_scripts_and_styles' );
