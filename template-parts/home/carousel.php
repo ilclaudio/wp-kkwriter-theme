61a8077fb5c52d37cb15a40183880e07e32940a1
ilclaudio <ilclaudio@gmail.com>
2024-07-13 19:57:37 +0200

    New-feature: New version of the slider

--- template-parts/home/carousel.php
+++ template-parts/home/carousel.php
@@ -5,53 +5,92 @@
  * @package KK_Writer_Theme
  */
 
-$contents     = KKW_ContentsManager::get_home_carousel_contents();
-$num_contents = count( $contents );
+ // Get wrapped items to show.
+$items     = KKW_ContentsManager::get_home_carousel_contents();
+$num_items = count( $items );
 
-if ( $num_contents > 0 ){
+if ( $num_items > 0 ){
 ?>
 
 	<div id="carouselExampleIndicators" class="carousel slide">
+
 		<!-- INDICATORS -->
 		<div class="carousel-indicators">
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			<?php
+				$count = 0;
+			 foreach ( $items as $item ){
+			?>
+			<button type="button"
+				data-bs-target="#carouselExampleIndicators"
+				data-bs-slide-to="<?php echo $count; ?>"
+				class="<?php if ( $count === 0 ) echo 'active'; ?>"
+				aria-current="<?php if ( $count === 0 ) echo 'true'; ?>"
+				aria-label="Slide <?php echo $count; ?>"></button>
+			<?php
+				$count++;
+			 }
+			?>
 		</div>
 
-		<!-- ITEMS -->
+		<!-- BEGIN slides -->
 		<div class="carousel-inner">
-
-		<?php
-		$first = true;
-		foreach ( $contents as $c ){
-		?>
-		<div class="carousel-item <?php if ( $first ) echo 'active'; ?>" >
-			<div class="card mb-3" style="width: 800; height: 400px; background-color:#f5f5dc">
-				<div class="row g-0">
-					<div class="col-md-4 g-0 p-0 m-0">
-						<img style="max-height: 400px" src="http://localhost/writer/wp-content/uploads/2024/04/tessere-alla-deriva-321457.jpg"  class="img-fluid rounded-start" alt="...">
-					</div>
-					<div class="col-md-8">
-						<div class="card-body">
-							<h5 class="card-title">Card title</h5>
-							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-							<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+			<?php
+			$first = true;
+			foreach ( $items as $item ) {
+				$img_id    = get_post_thumbnail_id( $item->id );
+				$img_array = wp_get_attachment_image_src( $img_id, 'carousel-item' );
+				$img_src   = $img_array ? $img_array[0] : '';
+				$img_alt   = get_post_meta( $img_id, '_wp_attachment_image_alt', true );
+				$img_alt   = $img_alt ? $img_alt : $item->title;
+				$item_url  = $item->detail_url;
+			?>
+				<!-- begin slide -->
+				<div class="carousel-item <?php if ( $first ) echo 'active'; ?>" >
+					<div class="card mb-3" style="width: 600; height: 300px; background-color:#f5f5dc">
+						<div class="row g-0">
+							<!-- slide image -->
+							<div class="col-md-4 g-0 p-0 m-0">
+								<a class="kkw_link" href="<?php echo esc_url( $item_url); ?>">
+									<img style="max-height: 300px" 
+									src="<?php echo esc_url( $img_src ); ?>"
+									class="img-fluid rounded-start p-3" 
+									alt="<?php echo esc_attr( $img_alt ); ?>">
+								</a>
+							</div>
+							<!-- slide text -->
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">
+										<a class="kkw_link" href="<?php echo esc_url( $item_url); ?>">
+											<b><?php echo esc_attr( $item->title ); ?></b>
+										</a>
+									</h5>
+									<p class="card-text">
+										<?php echo wp_kses_post( $item->description ); ?>
+									</p>
+									<p class="card-text">
+										<small class="text-body-secondary">
+											<a class="kkw_link"
+												href="<?php echo esc_url( $item_url); ?>"><?php echo __( 'Vai al libro...', 'kk_writer_theme' ); ?>
+											</a>
+										</small>
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+				<!-- end slide -->
+			<?php
+			$first = false;
+			}
+			?>
 		</div>
-		<?php
-		$first = false;
-		}
-		?>
+		<!-- END slides -->
 
 
-
-
-		</div>
-
-		<!-- BUTTONS 
+		<!-- BUTTONS -->
+		 <!--
 		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="visually-hidden">Previous</span>
@@ -61,7 +100,8 @@ if ( $num_contents > 0 ){
 			<span class="visually-hidden">Next</span>
 		</button>
 		-->
-	</div>
+
+	</div> <!-- carousel-->
 <?php
 }
 ?>
