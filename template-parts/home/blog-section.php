8de0eff48c4e3e106d64ca81d3284a219550bd17
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-07-15 17:48:53 +0200

    New feature: Added blog home section

---
+++ template-parts/home/blog-section.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * KK Writer Theme: The BLOG sections in the Home Page oif the site.
+ *
+ * @package KK_Writer_Theme
+ */
+
+?>
+
+<div id="fc_first_row" class="row mt-4 mb-1 fc-row">
+
+	<!-- Box 1 -->
+	<div class="col-sm-4">
+			<div class="card">
+					<img src="immagine1.jpg" class="card-img-top" alt="Immagine 1">
+					<div class="card-body">
+							<h5 class="card-title">Titolo Post 1</h5>
+							<p class="card-text">Breve descrizione del post 1.</p>
+					</div>
+			</div>
+	</div>
+	<!-- Box 2 -->
+	<div class="col-sm-4">
+			<div class="card">
+					<img src="immagine2.jpg" class="card-img-top" alt="Immagine 2">
+					<div class="card-body">
+							<h5 class="card-title">Titolo Post 2</h5>
+							<p class="card-text">Breve descrizione del post 2.</p>
+					</div>
+			</div>
+	</div>
+	<!-- Box 3 -->
+	<div class="col-sm-4">
+			<div class="card">
+					<img src="immagine3.jpg" class="card-img-top" alt="Immagine 3">
+					<div class="card-body">
+							<h5 class="card-title">Titolo Post 3</h5>
+							<p class="card-text">Breve descrizione del post 3.</p>
+					</div>
+			</div>
+	</div>
+
+</div> <!-- end row -->
