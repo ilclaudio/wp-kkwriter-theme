722ee7cb86c28d147b14dcfdf8293ffb7d122494
ilclaudio <ilclaudio@gmail.com>
2024-07-16 16:37:18 +0200

    New feature: Managed the Home voice of the menu

--- template-parts/header/main-menu.php
+++ template-parts/header/main-menu.php
@@ -8,22 +8,30 @@
 	// The items of the menu.
 	if ( $menu_id ) {
 		$menu_items = wp_get_nav_menu_items( $menu_id );
-		// title, url, ID
 ?>
 
 <div class="nav-scroller py-1 mb-3 border-bottom">
 	<nav class="nav nav-underline justify-content-between">
 		<?php
 		foreach ( $menu_items as $item ) {
-			$page_id  = KKW_PolylangManager::get_page_by_id( $item->object_id );
-			$page     = get_post( $page_id );
-			$active   = is_page( $page_id );
-			$page_url = get_permalink( $page_id );
-			// $parent_id = wp_get_post_parent_id();--> multilevels checks.
+			if ( $item->post_name !== 'home' ) {
+				// All menu items.
+				$page_id  = KKW_PolylangManager::get_page_by_id( $item->object_id );
+					$page     = get_post( $page_id );
+					$active   = is_page( $page_id );
+					$page_url = get_permalink( $page_id );
+					$title    = $page->post_title;
+				// $parent_id = wp_get_post_parent_id();--> multilevels checks.
+			} else {
+				// Only the Home Page.
+				$active   = is_home();
+				$page_url = KKW_PolylangManager::get_home_url();
+				$title    = 'Home';
+			}
 		?>
 			<a class="nav-item nav-link link-body-emphasis <?php if ( $active ) echo 'active'; ?>"
 				href="<?php echo $page_url; ?>">
-				<?php echo $page->post_title; ?>
+				<?php echo $title; ?>
 			</a>
 		<?php
 		}
@@ -32,5 +40,5 @@
 </div>
 
 <?php
-	} // If the menu exists.
+	}
 ?>
