3a901a2512ec1f789552f187e774f530666f6a7b
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-07-19 23:36:56 +0200

    mc

--- template-parts/home/featured-contents.php
+++ template-parts/home/featured-contents.php
@@ -216,7 +216,7 @@ if ( $event ) {
 							<div class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center
 									 link-body-emphasis text-decoration-none border-top">
 								<img src="<?php echo esc_url( $news_img_src ); ?>"
-									class="bd-placeholder-img"
+									class="bd-placeholder-img d-none d-lg-block"
 									width="<?php echo strval( KKW_SMALL_FEATURED_IMG_WIDTH ); ?>"
 									height="<?php echo strval( KKW_SMALL_FEATURED_IMG_HEIGHT ); ?>"
 									alt="<?php echo esc_attr( $news_img_alt ); ?>" />
@@ -250,7 +250,7 @@ if ( $event ) {
 							<div class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center
 									link-body-emphasis text-decoration-none border-top">
 								<img src="<?php echo esc_url( $event_img_src ); ?>"
-									class="bd-placeholder-img"
+									class="bd-placeholder-img d-none d-lg-block"
 									width="<?php echo strval( KKW_SMALL_FEATURED_IMG_WIDTH ); ?>"
 									height="<?php echo strval( KKW_SMALL_FEATURED_IMG_HEIGHT ); ?>"
 									alt="<?php echo esc_attr( $event_img_alt ); ?>" />
