3199ca3e5aef411fc82e3a666c8ace34ab70b64a
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-07-27 19:15:07 +0200

    New feature: Added pagination

---
+++ template-parts/common/pagination.php
@@ -0,0 +1,22 @@
+<?php
+ $the_query = $args['query'];
+?>
+
+<nav class="pagination-wrapper justify-content-center" aria-label="Navigazione centrata">
+	<div class="row pt-5" id='pagination_links'>
+		<?php
+		if ( $the_query ) {
+			$prev_label = '<svg class="icon icon-secondary" role="img" aria-labelledby="Chevron Left"><use href="' . get_template_directory_uri() . '/assets/svg/sprites.svg#it-chevron-left"></use></svg>';
+			$next_label = '<svg class="icon icon-secondary" role="img" aria-labelledby="Chevron Right"><use href="' . get_template_directory_uri() . '/assets/svg/sprites.svg#it-chevron-right"></use></svg>';
+			echo paginate_links(
+				array(
+					'total'     => $the_query->max_num_pages,
+					'prev_text' => $prev_label,
+					'next_text' => $next_label,
+					'type'      => 'list', // Use 'plain' or 'list'.
+				)
+			);
+		}
+		?>
+	</div>
+</nav>
