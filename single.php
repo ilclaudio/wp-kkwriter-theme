b85d858d5b3916e193107ebaa1473f05dfd2d381
ilclaudio <ilclaudio@gmail.com>
2024-08-02 19:48:41 +0200

    mc

--- single.php
+++ single.php
@@ -12,53 +12,75 @@ $section_description = '';
 ?>
 
 <main class="container">
-		<!-- BREADCRUMB -->
-		<?php get_template_part( 'template-parts/common/breadcrumb' ); ?>
 
-	<!-- BODY -->
-	<div class="container mt-2">
+	<!-- BREADCRUMB -->
+	<?php get_template_part( 'template-parts/common/breadcrumb' ); ?>
 
-		<!-- BANNER -->
-		<section class="row mb-2 py-4 primary-bg">
-			<h1><?php echo $section; ?></h1>
-			<?php
-				if ( $section_description ){
-			?>
-			<div class="col-12">
-				<div class="form-group col text-left mb-2">
-				<?php echo $section_description; ?>
+	<?php
+	while(have_posts()) {
+		the_post();
+	?>
+		<!-- BODY -->
+		<div class="container mt-2">
+
+			<!-- BANNER -->
+			<section class="row mb-2 py-4 primary-bg">
+				<h1><?php echo $section; ?></h1>
+				<?php
+					if ( $section_description ){
+				?>
+				<div class="col-12">
+					<div class="form-group col text-left mb-2">
+					<?php echo $section_description; ?>
+					</div>
 				</div>
+				<?php
+					}
+				?>
+			</section>
+
+			<div class="row">
+
+				<!-- NAVIGATION column-->
+				<aside class="col-md-2 border-end mb-5">
+
+					<div class="menu-title text-center text-color-secondary">
+						<?php echo __( 'Details' , 'kk_writer_theme' ); ?>
+					</div>
+					<div class="kkw_lateral_menu">
+						<ul class="nav flex-column nav-menu">
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="#">Descrizione</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Date e orari</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Luogo</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link disabled">Contatti</a>
+							</li>
+						</ul>
+					</div>
+				</aside>
+
+				<!-- CONTENT column-->
+				<article class="col-md-10" aria-label="<?php echo __( 'Article' , 'kk_writer_theme' ); ?>">
+					<?php the_content(); ?>
+				</article>
+
 			</div>
-			<?php
-				}
-			?>
-		</section>
-
-		<div class="row">
-
-			<!-- NAVIGATION column-->
-			<aside class="col-md-3 border-end mb-5">
-				The navigation menu
-			</aside>
-
-			<!-- CONTENT column-->
-			<article class="col-md-9" aria-label="<?php echo __( 'Article' , 'kk_writer_theme' ); ?>">
-					The article
-			</article>
 
 		</div>
-
-	</div>
+	<?php
+	}
+	?>
 
 </main>
 
-<!-- <?php
-while(have_posts()) {
-	the_post(); ?>
-	<h2><?php the_title(); ?></h2>
-	<?php the_content(); ?>
 	
-<?php } ?> -->
+
 
 
 
