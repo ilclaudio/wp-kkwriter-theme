93a87f8f5cfd825c050976dccddd1d40c62171e8
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-08-04 18:18:58 +0200

    Added the template of all the books iotems

---
+++ single-kkw_book.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * KK Writer Theme: The default page for: Articles, Events and News.
+ *
+ * @package KK_Writer_Theme
+ */
+  
+get_header();
+
+$section = '';
+$section_description = '';
+?>
+
+<main class="container">
+
+	<!-- BREADCRUMB -->
+	<?php get_template_part( 'template-parts/common/breadcrumb' ); ?>
+
+	BOOK
+
+</main>
+
+
+<?php
+get_footer();
