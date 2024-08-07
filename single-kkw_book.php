7bbb01ff0644188c7d35252666b14997b07462a2
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-08-07 09:23:06 +0200

    mc

--- single-kkw_book.php
+++ single-kkw_book.php
@@ -85,7 +85,7 @@ $icon_name           = 'fa-book';
 				<section class="col-md-10 pb-3" aria-label="<?php echo __( 'Article' , 'kk_writer_theme' ); ?>">
 
 					<!-- Image and description -->
-					<article class="row">
+					<section class="row" aria-label="<?php echo __( 'Image and description of the book' , 'kk_writer_theme' ); ?>">
 						<div class="col-12">
 							<div id="kkw_book_img_div" class="float-start me-3">
 								<a href="<?php echo esc_url( $image_wrapper->src ); ?>" data-lightbox="image-1">
@@ -98,27 +98,58 @@ $icon_name           = 'fa-book';
 							</div>
 							<?php the_content(); ?>
 						</div>
-					</article>
+					</section>
 					
-					<!-- Tabs -->
-					<section id="kkw_book_tabs" class="mt-0 pt-0 mb-5" style="min-height: 300px;">
-						<ul class="nav nav-tabs">
-							<li class="nav-item">
-								<a class="nav-link" aria-current="page" href="#">Info</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link active" href="#">Recensioni</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Testi</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link disabled" aria-disabled="true">Brani</a>
-							</li>
-						</ul>
+					<!-- Tabs header-->
+					<section id="kkw_book_tabs" class="mt-0 pt-0 mb-5" style="min-height: 300px;"
+						aria-label="<?php echo __( 'Tabs to switch among contents' , 'kk_writer_theme' ); ?>">
+						<nav>
+							<div class="nav nav-tabs" id="nav-tab" role="tablist">
+								<button class="nav-link active" id="nav-home-tab"
+									data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" 
+									aria-controls="nav-home" aria-selected="true">
+									<?php echo __( 'Information' , 'kk_writer_theme' ); ?>
+								</button>
+								<button class="nav-link" id="nav-profile-tab"
+									data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" 
+									aria-controls="nav-profile" aria-selected="false">
+									<?php echo __( 'Reviews' , 'kk_writer_theme' ); ?>
+								</button>
+								<button class="nav-link" id="nav-contact-tab"
+									data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab"
+									aria-controls="nav-contact" aria-selected="false">
+									<?php echo __( 'Lyrics' , 'kk_writer_theme' ); ?>
+								</button>
+								<button class="nav-link" id="nav-disabled-tab"
+									data-bs-toggle="tab" data-bs-target="#nav-disabled" type="button" role="tab"
+									aria-controls="nav-disabled" aria-selected="false" disabled>
+									<?php echo __( 'Tracks' , 'kk_writer_theme' ); ?>
+								</button>
+							</div>
+						</nav>
+						<!-- Tabs body -->
+						<div class="tab-content" id="nav-tabContent">
+							<div class="tab-pane fade show active" id="nav-home" role="tabpanel"
+								aria-labelledby="nav-home-tab" tabindex="0">
+							a
+							</div>
+							<div class="tab-pane fade" id="nav-profile" role="tabpanel"
+								aria-labelledby="nav-profile-tab" tabindex="0">
+							b
+							</div>
+							<div class="tab-pane fade" id="nav-contact" role="tabpanel"
+								aria-labelledby="nav-contact-tab" tabindex="0">
+							c
+							</div>
+							<div class="tab-pane fade" id="nav-disabled" role="tabpanel"
+								aria-labelledby="nav-disabled-tab" tabindex="0">
+							d
+							</div>
+						</div>
 					</section>
 
-				</section>
+
+				</section> <!-- content column -->
 
 			</div> <!-- row -->
 		</div> <!-- body -->
