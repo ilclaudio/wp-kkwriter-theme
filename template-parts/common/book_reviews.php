59123a412758cabc7d0378c54b6fa7c39539caee
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-08-08 08:51:08 +0200

    Added book sections

---
+++ template-parts/common/book_reviews.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * KK Writer Theme: The SECTION template.
+ *
+ * @package KK_Writer_Theme
+ */
+?>
+
+<div>
+<p>REVIEWS</p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales rutrum blandit. 
+Curabitur laoreet a neque at ornare. Quisque sem dui, efficitur et convallis nec, congue at leo.
+ Donec vitae pulvinar sapien. Aenean lorem nulla, placerat quis risus at, 
+ faucibus imperdiet turpis. Mauris pellentesque vel est eget efficitur. 
+ Phasellus viverra enim at laoreet suscipit. Aenean pulvinar aliquam aliquam. Cras vel felis quis tortor molestie vehicula ac quis enim. Donec vitae felis feugiat, imperdiet est a, consectetur quam. Suspendisse auctor dignissim tellus a posuere. Vestibulum suscipit ullamcorper enim, imperdiet pretium neque rutrum id. Nunc placerat bibendum ipsum nec mattis.
+
+Vivamus vel ipsum eget elit viverra laoreet. Aliquam sit amet tristique leo. 
+Suspendisse vitae mi quis dolor mattis cursus sed egestas lorem. Sed ac libero purus. 
+Vivamus quis lorem quis mauris vehicula sagittis. Proin aliquet ante id sagittis interdum. 
+In augue magna, congue non finibus ac, laoreet sed est. Curabitur orci dolor, convallis nec eros sed, maximus mattis augue. Mauris fermentum, est a vestibulum molestie, metus tellus sodales metus, vel vulputate elit purus sit amet neque.
+
+Mauris pellentesque faucibus leo eu dignissim. Phasellus ac tempor orci, consequat pharetra purus. 
+Aliquam vel ornare enim. Etiam pulvinar ornare porttitor. 
+Proin quis sem vel ex hendrerit mollis sit amet sit amet tortor. Proin efficitur et magna 
+malesuada rutrum. Mauris sed quam ac leo interdum tincidunt.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit. Phasellus at nulla vel justo suscipit tincidunt.
+ Integer vitae pretium erat, sit amet viverra erat. Nulla facilisi. Aliquam et aliquet purus, 
+ vitae pharetra sapien. Suspendisse a iaculis augue.
+</div>
---
+++ template-parts/common/book_tracks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * KK Writer Theme: The SECTION template.
+ *
+ * @package KK_Writer_Theme
+ */
+?>
+
+<div>
+<p>TRACKS</p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales rutrum blandit. 
+Curabitur laoreet a neque at ornare. Quisque sem dui, efficitur et convallis nec, congue at leo.
+ Donec vitae pulvinar sapien. Aenean lorem nulla, placerat quis risus at, 
+ faucibus imperdiet turpis. Mauris pellentesque vel est eget efficitur. 
+ Phasellus viverra enim at laoreet suscipit. Aenean pulvinar aliquam aliquam. Cras vel felis quis tortor molestie vehicula ac quis enim. Donec vitae felis feugiat, imperdiet est a, consectetur quam. Suspendisse auctor dignissim tellus a posuere. Vestibulum suscipit ullamcorper enim, imperdiet pretium neque rutrum id. Nunc placerat bibendum ipsum nec mattis.
+</div>
