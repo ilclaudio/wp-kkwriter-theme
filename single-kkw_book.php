2b26b073cd79ac9ecd7d236c545d5d4d5999dbf4
ilclaudio <ilclaudio@gmail.com>
2024-08-08 08:51:08 +0200

    Added book sections

--- single-kkw_book.php
+++ single-kkw_book.php
@@ -105,45 +105,81 @@ $icon_name           = 'fa-book';
 						aria-label="<?php echo __( 'Tabs to switch among contents' , 'kk_writer_theme' ); ?>">
 						<nav>
 							<div class="nav nav-tabs" id="nav-tab" role="tablist">
-								<button class="nav-link active" id="nav-home-tab"
-									data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" 
-									aria-controls="nav-home" aria-selected="true">
+								<button class="nav-link active" id="nav-info-tab"
+									data-bs-toggle="tab" data-bs-target="#nav-info" type="button" role="tab" 
+									aria-controls="nav-info" aria-selected="true">
 									<?php echo __( 'Information' , 'kk_writer_theme' ); ?>
 								</button>
-								<button class="nav-link" id="nav-profile-tab"
-									data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" 
-									aria-controls="nav-profile" aria-selected="false">
+								<button class="nav-link" id="nav-reviews-tab"
+									data-bs-toggle="tab" data-bs-target="#nav-reviews" type="button" role="tab" 
+									aria-controls="nav-reviews" aria-selected="false">
 									<?php echo __( 'Reviews' , 'kk_writer_theme' ); ?>
 								</button>
-								<button class="nav-link" id="nav-contact-tab"
-									data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab"
-									aria-controls="nav-contact" aria-selected="false">
+								<button class="nav-link" id="nav-lyrics-tab"
+									data-bs-toggle="tab" data-bs-target="#nav-lyrics" type="button" role="tab"
+									aria-controls="nav-lyrics" aria-selected="false">
 									<?php echo __( 'Lyrics' , 'kk_writer_theme' ); ?>
 								</button>
-								<button class="nav-link" id="nav-disabled-tab"
-									data-bs-toggle="tab" data-bs-target="#nav-disabled" type="button" role="tab"
-									aria-controls="nav-disabled" aria-selected="false" disabled>
+								<button class="nav-link" id="nav-tracks-tab"
+									data-bs-toggle="tab" data-bs-target="#nav-tracks" type="button" role="tab"
+									aria-controls="nav-tracks" aria-selected="false" disabled>
 									<?php echo __( 'Tracks' , 'kk_writer_theme' ); ?>
 								</button>
 							</div>
 						</nav>
 						<!-- Tabs body -->
 						<div class="tab-content" id="nav-tabContent">
-							<div class="tab-pane fade show active" id="nav-home" role="tabpanel"
-								aria-labelledby="nav-home-tab" tabindex="0">
-							a
+							<div class="tab-pane fade show active" id="nav-info" role="tabpanel"
+								aria-labelledby="nav-info-tab" tabindex="0">
+								<!-- Book Informations -->
+								<?php
+									get_template_part(
+										'template-parts/common/book_info',
+										null,
+										array(
+										'post_wrapper' => $post_wrapper,
+										)
+									);
+								?>
 							</div>
-							<div class="tab-pane fade" id="nav-profile" role="tabpanel"
-								aria-labelledby="nav-profile-tab" tabindex="0">
-							b
+							<div class="tab-pane fade" id="nav-reviews" role="tabpanel"
+								aria-labelledby="nav-reviews-tab" tabindex="0">
+								<!-- Book Reviews -->
+								<?php
+									get_template_part(
+										'template-parts/common/book_reviews',
+										null,
+										array(
+										'post_wrapper' => $post_wrapper,
+										)
+									);
+								?>
 							</div>
-							<div class="tab-pane fade" id="nav-contact" role="tabpanel"
-								aria-labelledby="nav-contact-tab" tabindex="0">
-							c
+							<div class="tab-pane fade" id="nav-lyrics" role="tabpanel"
+								aria-labelledby="nav-lyrics-tab" tabindex="0">
+								<!-- Book Reviews -->
+								<?php
+									get_template_part(
+										'template-parts/common/book_lyrics',
+										null,
+										array(
+										'post_wrapper' => $post_wrapper,
+										)
+									);
+								?>
 							</div>
-							<div class="tab-pane fade" id="nav-disabled" role="tabpanel"
-								aria-labelledby="nav-disabled-tab" tabindex="0">
-							d
+							<div class="tab-pane fade" id="nav-tracks" role="tabpanel"
+								aria-labelledby="nav-tracks-tab" tabindex="0">
+								<!-- Book Tracks -->
+								<?php
+									get_template_part(
+										'template-parts/common/book_tracks',
+										null,
+										array(
+										'post_wrapper' => $post_wrapper,
+										)
+									);
+								?>
 							</div>
 						</div>
 					</section>
