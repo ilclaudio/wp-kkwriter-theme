96930aae6326f454baf64e70baaf79eaeedd9d3f
ilclaudio <ilclaudio@gmail.com>
2024-08-12 18:08:45 +0200

    mc

--- assets/custom/js/main.js
+++ assets/custom/js/main.js
@@ -32,3 +32,13 @@ lightbox.option({
 	'wrapAround': true,
 	'showImageNumberLabel': true,
 });
+
+/* Select active link in navbars */
+let links = document.querySelectorAll('.nav-link');
+for(let i=0; i<links.length; i++){
+links[i].addEventListener('click', function() {
+	for(let j=0; j<links.length; j++)
+		links[j].classList.remove('active');
+	this.classList.add('active');
+});
+}
