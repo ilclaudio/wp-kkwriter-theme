75db963725f31f08dc4968b16556739c88e439c4
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-08-13 18:00:38 +0200

    mc

--- assets/custom/js/main.js
+++ assets/custom/js/main.js
@@ -34,31 +34,54 @@ lightbox.option({
 });
 
 /* Select active link in navbars */
-let links = document.querySelectorAll('.nav-link');
+let links = document.querySelectorAll('#kkw_lateral_menu .nav-link');
 for(let i=0; i<links.length; i++){
-links[i].addEventListener('click', function() {
-	for(let j=0; j<links.length; j++)
-		links[j].classList.remove('active');
-	this.classList.add('active');
-
-	// Code added to manage book tabs.
-	let tabs = ["#nav-info", "#nav-reviews", "#nav-excerpts", "#nav-excerpts"];
-	let anchor = this.getAttribute("href");
-	if (tabs.includes(anchor)) {
-		document.querySelectorAll('#nav-tab .nav-link.active').forEach(tab => {
-			tab.classList.remove('active');
-			tab.setAttribute('aria-selected', 'false');
-		});
-		document.querySelectorAll('#nav-tabContent .tab-pane').forEach(tab => {
-			tab.classList.remove('active');
-			tab.classList.remove('show');
-		});
-		let selector = '#nav-tab button[data-bs-target="' + anchor + '"]'
-		const btn = document.querySelector(selector);
-		let instance = null;
-		if (btn) instance = new bootstrap.Tab(btn);
-		if (instance) instance.show();
+links[i].addEventListener('click',
+	function() {
+		// console.log(wpMenuTabs);
+		for(let j=0; j<links.length; j++) {
+			links[j].classList.remove('active');
+		}
+		this.classList.add('active');
+		// Code added to manage book tabs.
+		// let wpMenuTabs = ["#nav-info", "#nav-reviews", "#nav-excerpts", "#nav-excerpts"];
+		let anchor = this.getAttribute("href");
+		if (wpMenuTabs.includes(anchor)) {
+			document.querySelectorAll('#nav-tab .nav-link.active').forEach(tab => {
+				tab.classList.remove('active');
+				tab.setAttribute('aria-selected', 'false');
+			});
+			document.querySelectorAll('#nav-tabContent .tab-pane').forEach(tab => {
+				tab.classList.remove('active');
+				tab.classList.remove('show');
+			});
+			let selector = '#nav-tab button[data-bs-target="' + anchor + '"]'
+			const btn = document.querySelector(selector);
+			let instance = null;
+			if (btn) instance = new bootstrap.Tab(btn);
+			if (instance) instance.show();
+		}
 	}
+	);
+}
 
-});
+let bookTabs = document.querySelectorAll('#kkw_book_tabs .nav-link');
+for(let i=0; i<bookTabs.length; i++){
+	bookTabs[i].addEventListener('click',
+		function() {
+			// let wpMenuTabs = ["#nav-info", "#nav-reviews", "#nav-excerpts", "#nav-excerpts"];
+			// console.log(wpMenuTabs);
+			let anchor = this.getAttribute("data-bs-target");
+			if (wpMenuTabs.includes(anchor)) {
+				const menuItems = document.querySelectorAll('#kkw_lateral_menu .nav-link');
+				menuItems.forEach(item => {
+					item.classList.remove('active');
+				});
+				const activeItem = document.querySelector(`#kkw_lateral_menu .nav-link[href="${anchor}"]`);
+				if (activeItem) {
+					activeItem.classList.add('active');
+				}
+			}
+		}
+	);
 }
