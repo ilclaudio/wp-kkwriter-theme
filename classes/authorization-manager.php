3f9897a812f59d933b0ca3ff6adc981fbc6501a8
ilclaudio <ilclaudio@gmail.com>
2026-03-28 11:44:21 +0100

    Fix authorization manager and add commit-msg hook

--- classes/authorization-manager.php
+++ classes/authorization-manager.php
@@ -1,46 +1,59 @@
 <?php
 /**
- * Definition of the Authorization Manager.
+ * KK Writer Theme: Authorization manager.
  *
  * @package KK_Writer_Theme
  */
+// phpcs:ignoreFile WordPress.Files.FileName.InvalidClassFileName
 
-
+/**
+ * Manage custom roles and authorization capabilities.
+ */
 class KKW_AuthorizationManager {
 	/**
 	 * Constructor of the Manager.
 	 */
 	public function __construct() {}
 
-	public function setup(){
+	/**
+	 * Register authorization hooks.
+	 *
+	 * @return void
+	 */
+	public function setup() {
 		// Register custom roles.
 		add_action( 'init', array( $this, 'add_super_editor' ) );
 	}
 
-	public function add_super_editor(){
+	/**
+	 * Add the custom super editor role and capabilities.
+	 *
+	 * @return void
+	 */
+	public function add_super_editor() {
 		// Get the base role (Editor).
-		$base_role = get_role( 'editor' );
-		// Check if the role exists.
+		$kkw_base_role = get_role( 'editor' );
+		if ( ! $kkw_base_role ) {
+			return;
+		}
 
-		if ( $base_role ) {
+		// Add a new role based on the Editor role, if not exists.
+		$kkw_new_role = get_role( KKW_SUPER_EDITOR_ROLE_SLUG );
+		if ( ! $kkw_new_role ) {
+			$kkw_new_role = add_role( KKW_SUPER_EDITOR_ROLE_SLUG, KKW_SUPER_EDITOR_ROLE_NAME, $kkw_base_role->capabilities );
+		}
 
-			// Add a new role based on the Editor role, if not exists.
-			$new_role = get_role( KKW_SUPER_EDITOR_ROLE_SLUG );
-			if ( ! $new_role ){
-				$new_role = add_role( KKW_SUPER_EDITOR_ROLE_SLUG, KKW_SUPER_EDITOR_ROLE_NAME, $base_role->capabilities );
-			}
-
-			// Assign the new role the permission to modify the theme and the site's menu (Appearance section of the back-office).
-			$new_role->add_cap( 'edit_theme_options' );
+		if ( $kkw_new_role ) {
+			// Allow the role to modify menus and theme options.
+			$kkw_new_role->add_cap( 'edit_theme_options' );
 			// Assign the permission to modify the configurations of the theme to the new role.
-			$new_role->add_cap( KKW_EDIT_CONFIG_PERMISSION );
+			$kkw_new_role->add_cap( KKW_EDIT_CONFIG_PERMISSION );
+		}
 
-			// Assign the permission to modify the theme configurations  to the site administrator.
-			$admin_role = get_role( 'administrator' );
-			if ( $admin_role ){
-				$admin_role->add_cap( KKW_EDIT_CONFIG_PERMISSION );
-			}
+		// Assign the permission to modify the theme configurations to the site administrator.
+		$kkw_admin_role = get_role( 'administrator' );
+		if ( $kkw_admin_role ) {
+			$kkw_admin_role->add_cap( KKW_EDIT_CONFIG_PERMISSION );
 		}
 	}
-
 }
